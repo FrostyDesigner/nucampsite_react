c6da97501361b2d2964f28d7058f6119c150250e
FrostyDesigner <drafter760@gmail.com>
2020-02-20 18:03:34 -0800

    updated "react-scripts" to 3.4 - bug resolved

--- src/shared/baseUrl.js
+++ src/shared/baseUrl.js
@@ -1,1 +1,3 @@
-export const baseUrl = 'http://localhost:3001/';
+// export const baseUrl = 'http://localhost:3001/';  //localhost but sometimes does not work
+// export const baseUrl = 'http://192.168.1.41:3001/' //nucamp @ Tacoma
+export const baseUrl = 'http://10.0.0.152:3001/';
